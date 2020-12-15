d48a53b2b9651fc6e9e000156ced395f49e873ed
George Abraham <george.abraham@incture.com>
2020-12-15 10:37:57 +0000

    Base Project for Neo

--- IMO_PM/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"com/sap/incture/IMO_PM/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/sap/incture/IMO_PM/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
