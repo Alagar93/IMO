7f7f77ba513f79bdb43147dcc4eb37e6992b87b0
George Abraham <george.abraham@incture.com>
2020-12-15 13:27:30 +0000

    Porting to Neo from CF - Copying Code Files

--- webapp/test/unit/AllTests.js
+++ webapp/test/unit/AllTests.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-	"com/sap/incture/IMO_PM/test/unit/controller/App.controller"
+	"com/sap/incture/IMO_PM/test/unit/controller/View1.controller"
 ], function () {
 	"use strict";
 });
