05e0760381fa03e6d0f8896faa77ae85d556cd8f
nischaljadhav <nischal.jadhav@incture.com>
2021-01-06 13:24:41 +0000

    BreakDown Duration Validation and Pop-Up ,Pop-up for Malfunation Start Date and Time exceeding current Date and time.

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -989,7 +989,7 @@ sap.ui.define([
 					}
 					//nischal -- starts -- downtime was embedded with additional empty strings
 					if(oData.Downtime){
-					var downTime = parseInt(oData.Downtime,10);
+					var downTime = parseFloat(oData.Downtime).toFixed(2);
 					oData.Downtime = downTime.toString();
 					}
 					if(oData.Breakdown === true){
